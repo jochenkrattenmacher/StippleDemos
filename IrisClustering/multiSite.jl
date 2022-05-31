d44032ffb04e900e2a6c3ee355e32122018b662c
Jochen Krattenmacher <jochen.krattenmacher@web.de>
2022-05-31 22:31:12 +0200

    chart is now reactive
    chart type selection "page"

--- IrisClustering/multiSite.jl
+++ IrisClustering/multiSite.jl
@@ -6,35 +6,56 @@ data = DataFrame(Costs = [44, 55, 13, 43, 22])
 labels = ["Team A", "Team B", "Team C", "Team D", "Team E"]
 chart_width = 500
 # data2 = DataFrame(Costs = [21, 99, 13, 21, 70])
+barchart = [PlotSeries("name", PlotData(data.Costs))]
+piechart = data.Costs
+plot_options_bar = PlotOptions(;
+    chart_type = :bar,
+    chart_width,
+    labels,
+)
+
+plot_options_pie = PlotOptions(;
+    chart_type = :pie,
+    chart_width,
+    chart_animations_enabled = true,
+    stroke_show = false,
+    labels,
+)
 
 @reactive! mutable struct Example <: ReactiveModel
-    plot_options_bar::R{PlotOptions} = PlotOptions(;
-        chart_type = :bar,
-        chart_width,
-        labels,
-        ), READONLY
-
-    plot_options_pie::R{PlotOptions} = PlotOptions(;
-        chart_type = :pie,
-        chart_width,
-        chart_animations_enabled = true,
-        stroke_show = false,
-        labels,
-      )
-    piechart_::R{Vector} = data.Costs
-    barchart::R{Vector{PlotSeries}} = [PlotSeries("name", PlotData(piechart_))]
+    plot_options::R{PlotOptions} = plot_options_pie
+    chart::R{Union{Vector,Vector{PlotSeries}}} = piechart
 
     drawer::R{Bool} = false
-    show_bar::R{Bool} = true
+    show_bar::R{Bool} = false
+    show_plot::R{Bool} = false
 end
 
 Stipple.register_components(Example, StippleCharts.COMPONENTS)
 model = Stipple.init(Example)
 
+function switch_plots(model)
+    println("hello")
+    if model.show_bar[]
+        model.plot_options[] = plot_options_bar
+        model.chart[] = barchart
+    else
+        model.plot_options[] = plot_options_pie
+        model.chart[] = piechart
+    end
+    return model
+end
+
 function handlers(model)
     on(model.isready) do ready
         ready || return
-        notify(model.plot_options_bar)        
+        notify(model.plot_options)        
+    end
+    on(model.show_bar) do _
+        switch_plots(model)     
+    end
+    on(model.show_plot) do _
+        "helloooooo"     
     end
     model
 end
@@ -61,11 +82,13 @@ function ui(model)
             ])
             h3("Example Plot")
             row(
-                StippleCharts.plot(:barchart, options = :plot_options_bar), @iif(:show_bar)
+                StippleCharts.plot(:chart, options = :plot_options), @iif(:show_plot)
             )
             row(
                 # cell(class = "st-module", [plot(:piechart_, options! = "plot_options")]),
-                StippleCharts.plot(:piechart_, options = :plot_options_pie), @els(:show_bar)
+                [radio("Pie plot", :show_bar, val = 0),
+                radio("Bar plot", :show_bar, val = "true"),
+                btn("Show plot", color = "secondary", @click("show_plot = true"))],@els(:show_plot)
             )
         ])
     ])
